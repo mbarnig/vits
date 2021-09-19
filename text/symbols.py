02a325fc60c9d2812740bfe06d9d2b6b464ec4df
Marco BARNIG <mbarnig@pt.lu>
2021-09-19 16:22:45 +0200

    Update symbols.py

--- text/symbols.py
+++ text/symbols.py
@@ -4,17 +4,13 @@
 Defines the set of symbols used in text input to the model.
 '''
 _pad        = '_'
-_tie = '\u0361'
-_nasal = '\u0303'
-_non_syllabic = '\u032f'
-_special_1 = '\u00e3'  # ã
 _punctuation = ';:,.!?¡¿—…"«»“” '
 _letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
 _letters_ipa = "ɑɐɒæɓʙβɔɕçɗɖðʤəɘɚɛɜɝɞɟʄɡɠɢʛɦɧħɥʜɨɪʝɭɬɫɮʟɱɯɰŋɳɲɴøɵɸθœɶʘɹɺɾɻʀʁɽʂʃʈʧʉʊʋⱱʌɣɤʍχʎʏʑʐʒʔʡʕʢǀǁǂǃˈˌːˑʼʴʰʱʲʷˠˤ˞↓↑→↗↘'̩'ᵻ"
 
 
 # Export all symbols:
-symbols = [_pad] + list(_punctuation) + list(_letters) + list(_letters_ipa) + [_tie] + [_nasal] + [_non_syllabic] + [_special_1]
+symbols = [_pad] + list(_punctuation) + list(_letters) + list(_letters_ipa)
 
 # Special symbol ids
 SPACE_ID = symbols.index(" ")
