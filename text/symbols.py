caa5792d6d817127c59e59a9e270f931d8d01a98
Marco BARNIG <mbarnig@pt.lu>
2021-09-24 14:23:02 +0200

    Add files via upload

--- text/symbols.py
+++ text/symbols.py
@@ -8,13 +8,14 @@ _tie = '\u0361'
 _nasal = '\u0303'
 _non_syllabic = '\u032f'
 _special_1 = '\u00e3'  # ã
+_special_2 = '\u00f9'  # õ
 _punctuation = ';:,.!?¡¿—…"«»“” '
 _letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
 _letters_ipa = "ɑɐɒæɓʙβɔɕçɗɖðʤəɘɚɛɜɝɞɟʄɡɠɢʛɦɧħɥʜɨɪʝɭɬɫɮʟɱɯɰŋɳɲɴøɵɸθœɶʘɹɺɾɻʀʁɽʂʃʈʧʉʊʋⱱʌɣɤʍχʎʏʑʐʒʔʡʕʢǀǁǂǃˈˌːˑʼʴʰʱʲʷˠˤ˞↓↑→↗↘'̩'ᵻ"
 
 
 # Export all symbols:
-symbols = [_pad] + list(_punctuation) + list(_letters) + list(_letters_ipa) + [_tie] + [_nasal] + [_non_syllabic] + [_special_1]
+symbols = [_pad] + list(_punctuation) + list(_letters) + list(_letters_ipa) + [_tie] + [_nasal] + [_non_syllabic] + [_special_1] + [_special_2]
 
 # Special symbol ids
 SPACE_ID = symbols.index(" ")
